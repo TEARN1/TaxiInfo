1d1642eeb1eb38ddc6716a8a2a66be80e28f43e3
Asemahle Nkwali <99318798+TEARN1@users.noreply.github.com>
2024-01-25 14:10:30 +0200

    Add files via upload
    25 Jan 2024 update

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ function Transport() {
     document.getElementById("CvA").style.display = "none";
     document.getElementById("EduA").style.display = "none";
 
+
     // Nav bar styling
 
     // transport
@@ -43,12 +44,12 @@ function Education() {
     document.getElementById("EduA").style.display = "block";
     document.getElementById("CvA").style.display = "none";
     document.getElementById("Start").style.display = "none";
-    
-    
+
+
     // Nav bar styling
 
     document.getElementById("Education").style.backgroundColor = "rgb(0, 0, 255,0.5)"
-    document.getElementById("Education").style.color = "rgb(3, 255, 255)";
+    document.getElementById("Education").style.color = "white";
 
     // Transport
     document.getElementById("Transport").style.backgroundColor = "rgb(0, 0, 0)"
@@ -59,148 +60,411 @@ function Education() {
     document.getElementById("Send").style.color = "rgb(3, 255, 255)";
 }
 
-function grade12falp1november2022() {
+    // grade12falp2november2022
+
+function grade12falp2november2022() {
     var score = 0;
-    if (document.getElementById("correct1.1.1").checked) {
+    if (document.getElementById("grade12falp2november2022127").checked) {
+
         score++;
     }
-    if (document.getElementById("Question1.1.2").checked) {
+    if (document.getElementById("grade12falp2november2022126").checked) {
         score++;
     }
-    if (document.getElementById("Question1.2").checked) {
+    if (document.getElementById("grade12falp2november2022125").checked) {
         score++;
     }
-    if (document.getElementById("Question1.3.1").checked) {
+    if (document.getElementById("grade12falp2november2022124").checked) {
         score++;
     }
-    if (document.getElementById("Question1.3.2").checked) {
+    if (document.getElementById("grade12falp2november2022123").checked) {
         score++;
     }
-    if (document.getElementById("Question1.3.3").checked) {
+    if (document.getElementById("grade12falp2november2022122").checked) {
         score++;
     }
-    if (document.getElementById("Question1.4").checked) {
+    if (document.getElementById("grade12falp2november2022121").checked) {
         score++;
     }
-    if (document.getElementById("Question1.5.1").checked) {
+    if (document.getElementById("grade12falp2november2022120").checked) {
         score++;
     }
-    if (document.getElementById("Question1.5.2").checked) {
+    if (document.getElementById("grade12falp2november2022119").checked) {
         score++;
     }
-    if (document.getElementById("Question1.6").checked) {
+    if (document.getElementById("grade12falp2november2022118").checked) {
         score++;
     }
-    if (document.getElementById("Question1.7").checked) {
+    if (document.getElementById("grade12falp2november2022117").checked) {
         score++;
     }
-    if (document.getElementById("Question1.8").checked) {
+    if (document.getElementById("grade12falp2november2022116").checked) {
         score++;
     }
-    if (document.getElementById("Question1.9").checked) {
+    if (document.getElementById("grade12falp2november2022115").checked) {
         score++;
     }
-    if (document.getElementById("Question1.10").checked) {
+    if (document.getElementById("grade12falp2november2022114").checked) {
         score++;
     }
-    if (document.getElementById("Question1.11").checked) {
+    if (document.getElementById("grade12falp2november2022113").checked) {
         score++;
     }
-    if (document.getElementById("Question1.12").checked) {
+    if (document.getElementById("grade12falp2november2022112").checked) {
         score++;
     }
-    if (document.getElementById("Question1.13").checked) {
+    if (document.getElementById("grade12falp2november2022111").checked) {
         score++;
     }
-    if (document.getElementById("Question3.1").checked) {
+    if (document.getElementById("grade12falp2november2022110").checked) {
         score++;
     }
-    if (document.getElementById("Question3.2").checked) {
+    if (document.getElementById("grade12falp2november2022109").checked) {
         score++;
     }
-    if (document.getElementById("Question3.3").checked) {
+    if (document.getElementById("grade12falp2november2022108").checked) {
         score++;
     }
-    if (document.getElementById("Question3.4").checked) {
+    if (document.getElementById("grade12falp2november2022107").checked) {
         score++;
     }
-    if (document.getElementById("Question3.5").checked) {
+    if (document.getElementById("grade12falp2november2022106").checked) {
         score++;
     }
-    if (document.getElementById("Question3.6").checked) {
+    if (document.getElementById("grade12falp2november2022105").checked) {
         score++;
     }
-    if (document.getElementById("Question3.7").checked) {
+    if (document.getElementById("grade12falp2november2022104").checked) {
         score++;
     }
-    if (document.getElementById("Question4.1").checked) {
+    if (document.getElementById("grade12falp2november2022103").checked) {
         score++;
     }
-    if (document.getElementById("Question4.2").checked) {
+    if (document.getElementById("grade12falp2november2022102").checked) {
         score++;
     }
-    if (document.getElementById("Question4.3").checked) {
+    if (document.getElementById("grade12falp2november2022101").checked) {
         score++;
     }
-    if (document.getElementById("Question4.4").checked) {
+    if (document.getElementById("grade12falp2november2022100").checked) {
         score++;
     }
-    if (document.getElementById("Question4.5").checked) {
+    if (document.getElementById("grade12falp2november202299").checked) {
         score++;
     }
-    if (document.getElementById("Question4.6").checked) {
+    if (document.getElementById("grade12falp2november202298").checked) {
         score++;
     }
-    if (document.getElementById("Question4.7").checked) {
+    if (document.getElementById("grade12falp2november202297").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.1a").checked) {
+    if (document.getElementById("grade12falp2november202296").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.1b").checked) {
+    if (document.getElementById("grade12falp2november202295").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.1c").checked) {
+    if (document.getElementById("grade12falp2november202294").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.1d").checked) {
+    if (document.getElementById("grade12falp2november202293").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.2").checked) {
+    if (document.getElementById("grade12falp2november202292").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.3").checked) {
+    if (document.getElementById("grade12falp2november202291").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.4").checked) {
+    if (document.getElementById("grade12falp2november202290").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.5").checked) {
+    if (document.getElementById("grade12falp2november202289").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.6").checked) {
+    if (document.getElementById("grade12falp2november202288").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.7").checked) {
+    if (document.getElementById("grade12falp2november202287").checked) {
         score++;
     }
-    if (document.getElementById("Question5.1.8").checked) {
+    if (document.getElementById("grade12falp2november202286").checked) {
         score++;
     }
-    if (document.getElementById("Question5.2.1").checked) {
+    if (document.getElementById("grade12falp2november202285").checked) {
         score++;
     }
-    if (document.getElementById("Question5.2.2").checked) {
+    if (document.getElementById("grade12falp2november202284").checked) {
         score++;
     }
-    if (document.getElementById("Question5.2.3").checked) {
+    if (document.getElementById("grade12falp2november202283").checked) {
         score++;
     }
-    if (document.getElementById("Question5.2.4").checked) {
+    if (document.getElementById("grade12falp2november202282").checked) {
         score++;
     }
-    if (document.getElementById("Question5.2.5").checked) {
+    if (document.getElementById("grade12falp2november202281").checked) {
         score++;
     }
-    document.write("Your Score is :" + score);
+    if (document.getElementById("grade12falp2november202280").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202279").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202278").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202277").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202276").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202275").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202274").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202273").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202272").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202271").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202270").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202269").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202268").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202267").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202266").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202265").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202264").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202263").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202262").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202261").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202260").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202259").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202258").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202257").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202256").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202255").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202254").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202253").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202252").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202251").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202250").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202249").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202248").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202247").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202246").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202245").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202244").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202243").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202242").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202241").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202240").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202239").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202238").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202237").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202236").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202235").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202234").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202233").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202232").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202231").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202230").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202229").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202228").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202227").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202226").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202225").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202224").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202223").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202222").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202221").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202220").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202219").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202218").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202217").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202216").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202215").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202214").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202213").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202212").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202211").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november202210").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20229").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20228").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20227").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20226").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20225").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20224").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20223").checked) {
+        score++;
+    }
+
+    if (document.getElementById("grade12falp2november20221").checked) {
+        score++;
+    }
+    if (document.getElementById("grade12falp2november20222").checked) {
+        score++;
+    }
+
+    document.getElementById("sub").style.display = "block";
+    document.getElementById("Score").innerHTML = score;
+}
+
+function subClose() {
+    document.getElementById("subTwo").style.display = "block";
+    document.getElementById("subWhere").style.display = "none";
+    document.getElementById("subScore").style.display = "none";
+}
+
+function subDidYouYes() {
+    document.getElementById("subTwo").style.display = "block";
+    document.getElementById("subDidYou").style.display = "none";
+    document.getElementById("subScore").style.display = "none";
+    document.getElementById("subWhere").style.display = "block";
+}
+
+function subDidYouNo() {
+    document.getElementById("sub").style.display = "none";
 }
